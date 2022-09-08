a9fe7370ee3e13fe5402650d6082d18256d01fa4
thatunkownguy <111831937+thatunkownguy@users.noreply.github.com>
2022-09-08 12:21:04 +1200

    Add files via upload

---
+++ lonewolf.py
@@ -0,0 +1,17 @@
+# cod to learn about variables
+
+# variables can be a string
+greeting = "hello world"
+
+# they can also be numebers
+fav_num = 7 
+print(fav_num * 2)
+
+#this variable has a problem
+my_name = "Cruz Cresswell boi"
+ 
+print(greeting)
+print(fav_num)
+print(my_name)
+
+print("My name is {}.  My favourite number is {}".format(my_name, fav_num))
