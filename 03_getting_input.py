bcca0b68121362cbcb23266c8093a175fe4d1da8
thatunkownguy <111831937+thatunkownguy@users.noreply.github.com>
2022-09-12 09:50:27 +1200

    Add files via upload

---
+++ 03_getting_input.py
@@ -0,0 +1,20 @@
+
+Username = input("what is your name? ")
+
+
+Fav_num = int(input("favrourite number?"))
+
+
+Double = Fav_num * 2
+half = Fav_num / 2
+Squared = Fav_num * Fav_num
+
+print ()
+
+print("hi {}, you favourite number is {}" .format(Username, Fav_num))
+print ()
+
+
+print("double {} is {}".format(Fav_num, Double))
+print("half {} is {}".format(Fav_num, Squared))
+print ()
